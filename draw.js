2ca25afdbeca06f7a893e7a2565565b92f37c544
Zack Fisher <zackfisher27@proton.me>
2022-08-26 23:56:04 -0400

    fixed bug with textbox margins, worked on animations TODO: complete textbox anims

--- draw.js
+++ draw.js
@@ -2,14 +2,20 @@
 
 function drawTextbox(whichChar){
     let target = whichChar.div;
-    console.log(whichChar);
+    console.log(window.getComputedStyle(whichChar.div).top);
 
     let textbox = document.createElement("div");
     textbox.className = "textbox";
 
     let text = document.createElement("p");
     text.innerHTML = whichChar.text;
+    text.style.margin = 0;
+
+    textbox.addEventListener("animationend", () => {
+        //somehow need to keep parent and child ref to delete the textbox here with node.deleteelm()
+    });
 
     textbox.append(text);
-    target.append(textbox);
+    target.appendChild(textbox);
+    console.log(window.getComputedStyle(whichChar.div).top)
 }
