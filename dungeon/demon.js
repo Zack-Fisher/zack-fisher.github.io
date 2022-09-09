6b5a6b08a92209ece7d401feec8522bad1b22b56
Zack Fisher <zackfisher27@proton.me>
2022-09-08 23:58:11 -0400

    demon datastruct

---
+++ dungeon/demon.js
@@ -0,0 +1,7 @@
+//a file for storing player demons.
+
+//basic data structure for a demon.
+const test = {shape: "sphere", color: "blue", attribute: null, summoned: false};
+
+//render this list in a window
+demonList = [test, test, test, test];
