84bcf9d505e4925261c3f8fdff1f7746b946b0d9
Zack-Fisher <zackfisher27@proton.me>
2022-09-15 23:55:32 -0400

    who even CARES anymore bro

---
+++ dungeonagain/etc/stats.js
@@ -0,0 +1,12 @@
+//controller for constantly updating statistics
+//comp status, demon status, etc
+
+//all in percentages
+var compStatus = {
+    cpu: 0.0,
+    ram: 0.0,
+};
+
+var demonList = {
+    demons: [],
+};
